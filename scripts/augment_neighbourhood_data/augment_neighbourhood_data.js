952054780ab8235090043335d83cb6ab135a5a5e
Rocky Petkov <rocky.petkov@yandex.com>
2023-01-02 01:32:11 -0500

    More work with data and doing interesting things with scripts

--- scripts/augment_neighbourhood_data/augment_neighbourhood_data.js
+++ scripts/augment_neighbourhood_data/augment_neighbourhood_data.js
@@ -4,8 +4,7 @@ const path = require('path');
 
 const DATA_WE_CARE_ABOUT = {
     "Population, 2016": "POPULATION",
-    "Land area in square kilometres": "AREA",
-    "Population density per square kilometre": "DENSITY",
+    "Land area in square kilometres": "LAND_AREA",
     "Occupied private dwellings by structural type of dwelling": "RESIDENTIAL_DWELLINGS",
     "Single-detached house": "SINGLE_FAMILY_DWELLINGS",
     "Apartment in a building that has five or more storeys": "MID_HIGH_RISE_APARTMENTS",
@@ -41,9 +40,14 @@ function main() {
         if (DATA_WE_CARE_ABOUT[characteristic]) {
             for (const neighbourhood of neighbourhoodsGeo.features) {
                 neighbourhoodId = String(neighbourhood.properties["AREA_SHORT_CODE"]);
-                neighbourhood.properties[DATA_WE_CARE_ABOUT[characteristic]] = parseFloat(category[neighbourhoodsMap.get(neighbourhoodId)]);
+                neighbourhood.properties[DATA_WE_CARE_ABOUT[characteristic]] = Number(category[neighbourhoodsMap.get(neighbourhoodId)].replace(/,/g, ''));
             }
         }
+    }
+
+    // The density numbers provided are not actually floating point numbers. Imagine.
+    for (const neighbourhood of neighbourhoodsGeo.features) {
+        neighbourhood.properties["DENSITY"] = neighbourhood.properties["POPULATION"] / neighbourhood.properties["LAND_AREA"];
     }
 
     fs.writeFileSync(path.join('.', 'data', '140NeighbourhoodsAugmented.geojson'), JSON.stringify(neighbourhoodsGeo));
